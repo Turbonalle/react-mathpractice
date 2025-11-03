b5646234670a93e7b3a2ae5a534e08cafc9ea6fe
Turbonalle <jonathanbagger@hotmail.com>
2025-11-03 22:34:15 +0200

    Automatic base change, for deployment and dev

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/math-practice',
+  base: process.env.NODE_ENV === "production" ? "/math-practice" : "/",
   plugins: [
     react(),
     tailwindcss(),
